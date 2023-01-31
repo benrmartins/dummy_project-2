bfe9d195d5d500e54aaea949ab4bc8f0bbac1561
benrmartins <benrmartins@gmail.com>
2023-01-30 19:33:06 -0500

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+// Writes the message "Hello, World" to standard output.
+
+import stdlib.StdOut;
+
+public class HelloWorld {
+    // Entry point.
+    public static void main(String[] args) {
+        StdOut.println("Hello, World");
+    }
+}
